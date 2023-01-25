a4674d4984f914e3ea1b6c2805da068380313aef
Musanzi <musanziw.m@gmail.com>
2023-01-25 17:22:16 +0200

    [CREATE] Add hero component.

---
+++ resources/js/Components/Hero.jsx
@@ -0,0 +1,13 @@
+export default function () {
+    return (
+        <section className='h-screen bg-hero bg-left-bottom bg-cover bg-no-repeat'>
+            <div className='h-full bg-dark-navy bg-opacity-95'>
+                <div className='h-full flex flex-col items-center justify-center pt-20'>
+                    <p className='text-3xl font-courgette text-green mb-3'>Bonjour je suis,</p>
+                    <h1 className='text-4xl font-bold text-slate mb-5'>Wilfried Musanzi</h1>
+                    <p className='text-2xl font-light'>Développeur web FullStack.</p>
+                </div>
+            </div>
+        </section>
+    )
+}
