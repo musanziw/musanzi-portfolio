0c3c2d4c0c01fa2dbb0aea7a84391fc4265d2566
Musanzi <musanziw.m@gmail.com>
2023-01-25 17:22:44 +0200

    [ADD] Add custom colors.

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,10 +12,26 @@ module.exports = {
     theme: {
         extend: {
             fontFamily: {
-                sans: ['Nunito', ...defaultTheme.fontFamily.sans],
+                roboto: ['Roboto', ...defaultTheme.fontFamily.sans],
+                courgette: ['Courgette', ...defaultTheme.fontFamily.sans],
             },
+            backgroundImage: {
+                'hero': "url('/imgs/hero.png')",
+                'section': "url('/imgs/section.png')",
+            },
+            colors: {
+                'navy': '#0a192f',
+                'dark-navy': '#020c1b',
+                'light-navy': '#112240',
+                'lightest-navy': '#233554',
+                'navy-shadow': 'rgba(2,12,27,0.7)',
+                'slate': '#8892b0',
+                'light-slate': '#a8b2d1',
+                'lightest-slate': '#ccd6f6',
+                'green': '#64ffda',
+                'green-tint': 'rgba(100,255,218,0.1)',
+            }
         },
     },
 
-    plugins: [require('@tailwindcss/forms')],
 };
